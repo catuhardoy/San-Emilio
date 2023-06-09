0ae323977251013714a60cf239c8dca6f30f0088
Pamela Herrera <marcelo@MacBook-Pro.local>
2023-06-09 17:02:33 -0300

    changes in caravan model and controllers

--- api/src/controllers/caravanControllers/caravanController.js
+++ api/src/controllers/caravanControllers/caravanController.js
@@ -5,13 +5,24 @@ const getCaravan = async( )=>{
     return caravan;
 };
 
-const createCaravan = async (name) => {
-    const caravan = new Caravan({name});
+const getCaravanById = async(_id)=>{
+    const caravan = await Caravan.findById( _id );
+    if(!caravan) throw error("Couldn't find a caravan for " + _id);
+    return caravan;
+};
+
+const createCaravan = async (name, isOwn, color, date) => {
+    const caravan = new Caravan({name, isOwn, color, date});
     await caravan.save();
     return caravan;
-}
+};
+
+
+
+
 
 module.exports = {
     getCaravan,
+    getCaravanById,
     createCaravan
 }
